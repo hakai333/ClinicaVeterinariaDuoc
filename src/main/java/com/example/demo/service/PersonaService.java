ef8b3dd09be7924cef5075f129bf91d4e74c00dc
unknown <CETECOM@AOAWSB02LC0729.duoc.acad>
2025-05-12 20:07:35 -0400

    Primer commit, se sube archivo

---
+++ src/main/java/com/example/demo/service/PersonaService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Persona;
+import com.example.demo.repository.PersonaRepository;
+
+@Service
+public class PersonaService {
+    @Autowired
+    private PersonaRepository personaRepository;
+
+
+    public String almacenar(Persona persona) { 
+        personaRepository.save(persona);
+        return "Persona almacenada";
+    }
+
+    public List<Persona> listar() {
+        return personaRepository.findAll();
+    }
+
+}
